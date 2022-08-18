7ee0dc89c6df29c118e0202cf36e88af9a040aac
hieu130 <hieuvanphan275@gmail.com>
2022-08-18 10:53:09 +0700

    update

--- src/main/java/com/example/client/controller/EmployeesController.java
+++ src/main/java/com/example/client/controller/EmployeesController.java
@@ -1,16 +1,18 @@
 package com.example.client.controller;
 
-import ch.qos.logback.core.net.server.Client;
 import com.example.client.model.Employees;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.glassfish.jersey.client.ClientConfig;
 import org.glassfish.jersey.logging.LoggingFeature;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -19,30 +21,24 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+@Controller
 public class EmployeesController {
     private final String REST_API_LIST = "http://localhost:8080/api/v1/employees";
     private final String REST_API_CREATE = "http://localhost:8080/api/v1/employees/saveemployees";
-    private final String REST_API_DELETE = "http://localhost:8080/api/v1/employees/updateemployees";
+    private final String REST_API_UPDATE = "http://localhost:8080/api/v1/employees/updateemployees";
 
-    @GetMapping(value = {"/" , "/list"})
+    @GetMapping(value = {"/" , "/listemployees"})
     public String index(Model model){
-        Client client = createJerseyRestClient();
+        javax.ws.rs.client.Client client = createJerseyRestClient();
         WebTarget target = client.target(REST_API_LIST);
         List<Employees> ls =  target.request(MediaType.APPLICATION_JSON_TYPE).get(List.class);
 
-        model.addAttribute("list" , ls);
-        return "list";
+        model.addAttribute("listEmployees" , ls);
+        return "listEmployees";
 
     }
 
-    @GetMapping("delete")
-    public String deleteUser( Integer id){
-        Client client = createJerseyRestClient();
-        WebTarget target = client.target(REST_API_DELETE + id);
-        Response response = target.request(MediaType.APPLICATION_JSON_TYPE).delete();
-        return "redirect:/";
-    }
-    private static Client createJerseyRestClient() {
+    private static javax.ws.rs.client.Client createJerseyRestClient() {
         ClientConfig clientConfig = new ClientConfig();
 
         // Config logging for client side
@@ -55,31 +51,29 @@ public class EmployeesController {
 
         return ClientBuilder.newClient(clientConfig);
     }
-    @GetMapping(value = "create")
-    public String createNewUsers(){
+    @GetMapping(value = "createNewEmployees")
+    public String createNewEmployees(){
         return "create";
     }
 
-    @PostMapping("save")
-    public String saveUser(@RequestParam String name,
-                           @RequestParam String email,
-                           @RequestParam int phone){
-        User u = new User();
-        u.setName(name);
-        u.setEmail(email);
-        u.setPhone(phone);
+    @PostMapping("saveemployees")
+    public String saveEmployees(@RequestParam String name,
+                           @RequestParam String wage){
+        Employees e = new Employees();
+        e.setName(name);
+        e.setWage(wage);
 
-        String jsonUser = convertToJson(u);
+        String jsonEmployees = convertToJson(e);
         Client client = createJerseyRestClient();
         WebTarget target = client.target(REST_API_CREATE);
         Response response = target.request(MediaType.APPLICATION_JSON_TYPE)
-                .post(Entity.entity(jsonUser , MediaType.APPLICATION_JSON));
+                .post(Entity.entity(jsonEmployees , MediaType.APPLICATION_JSON));
         return "redirect:/";
     }
-    private static String convertToJson(User user) {
+    private static String convertToJson(Employees employees) {
         ObjectMapper mapper = new ObjectMapper();
         try {
-            return mapper.writeValueAsString(user);
+            return mapper.writeValueAsString(employees);
         } catch (IOException e) {
             e.printStackTrace();
         }
